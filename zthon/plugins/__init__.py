b509f460e99cfaba1635d3d37fb3ae5f8bd909d8
روجر <104784913+rogerpq@users.noreply.github.com>
2023-07-08 20:41:38 +0300

    Update __init__.py

--- zthon/plugins/__init__.py
+++ zthon/plugins/__init__.py
@@ -18,7 +18,7 @@ from ..core.session import zedub
 from ..helpers import *
 from ..helpers.utils import _zedtools, _zedutils, _format, install_pip, reply_id
 from ..sql_helper.globals import gvarstatus
-from zthon.helpers.functions.musictool import 
+from zthon.helpers.functions.musictool import song_download
 
 # =================== CONSTANT ===================
 bot = zedub
